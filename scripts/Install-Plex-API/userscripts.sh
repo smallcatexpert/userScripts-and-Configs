a2f2c94117f80a4e29afdea1faf156b925d92f09
smallcatexpert <ahrob97@icloud.com>
2026-08-03 20:30:34 -0400

    Update userscripts.sh

--- scripts/Install-Plex-API/userscripts.sh
+++ scripts/Install-Plex-API/userscripts.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-pip install PlexAPI
-pip install python-dotenv
-pip install SQLAlchemy
+pip install num2words==0.5.14
+pip install pillow==11.2.1
+pip install PlexAPI==4.16.1
+pip install kometautils==0.3.4
+pip install requests==2.33.0
+pip install retrying==1.4.2
+pip install tqdm==4.67.1
