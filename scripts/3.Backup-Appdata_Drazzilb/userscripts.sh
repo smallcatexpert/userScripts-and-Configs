c4b7d63ac9126d3f3acacce1988eb8172f149194
smallcatexpert <ahrob97@icloud.com>
2026-08-03 20:54:16 -0400

    Update userscripts.sh

--- scripts/3.Backup-Appdata_Drazzilb/userscripts.sh
+++ scripts/3.Backup-Appdata_Drazzilb/userscripts.sh
@@ -7,6 +7,7 @@
 #       /_/    \_\ .__/| .__/ \__,_|\__,_|\__\__,_| |____/ \__,_|\___|_|\_\\__,_| .__/
 #                | |   | |                                                      | |
 #                |_|   |_|                                                      |_|
+#
 echo Pulling latest version of Appdata Backup
 /usr/bin/wget https://raw.githubusercontent.com/Drazzilb08/extra-scripts/refs/heads/main/backup_appdata.sh -O /mnt/user/appdata/scripts/backup-appdata/backup_appdata.sh
 echo Starting Appdata Backup
